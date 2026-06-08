bdae0a066c203f5c8cc5f5f978d40114d8178e71
Taraka <58762272+TarakaRevu@users.noreply.github.com>
2026-06-08 15:37:11 +0530

    Remove unsupported parameter checks in _scaled_mm
    Removed unsupported parameter checks for scale_a, scale_b, and bias in _scaled_mm function.

--- torch_spyre/_inductor/lowering.py
+++ torch_spyre/_inductor/lowering.py
@@ -249,12 +249,11 @@ def lower_scaled_mm(
     out_dtype=None,
     use_fast_accum=False,
 ):
-    if scale_a is not None:
-        raise Unsupported("scale_a parameter in _scaled_mm is not yet supported")
-    if scale_b is not None:
-        raise Unsupported("scale_b parameter in _scaled_mm is not yet supported")
+    
     if bias is not None:
-        raise Unsupported("bias parameter in _scaled_mm is not yet supported")
+        bias.realize()
+        result = lowering.add(result, bias)
+        result.realize()
     if scale_result is not None:
         raise Unsupported("scale_result parameter in _scaled_mm is not yet supported")
     if use_fast_accum:
